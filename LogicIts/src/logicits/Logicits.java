3403c91a01f59e32e294d386e665b394e6cf3938
fabio <fabio.henrique.gil@gmail.com>
2016-08-18 15:48:52 -0300

    javaccList(Refactor) -> A classe Sintatico recebe uma String como entrada e cria um objeto que contem um List<String> lista, no qual quarda tokens com garantia de corretude de sintaxe!

---
+++ LogicIts/src/logicits/Logicits.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicits;
+
+import logicits.javaccList.Sintatico;
+
+/**
+ *
+ * @author fabio
+ */
+public class Logicits {
+    public static void main(String[] args) {
+        Sintatico s = new Sintatico("p < q");
+    }
+}
---
+++ LogicIts/src/logicits/javaccList/Sintatico.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicits.javaccList;
+
+import com.sun.imageio.plugins.common.InputStreamAdapter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import sun.net.www.content.text.PlainTextInputStream;
+
+/**
+ *
+ * @author fabio
+ */
+public class Sintatico {
+
+    public List<String> lista = new ArrayList();
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Sintatico sint = new Sintatico("p <-> q");
+        
+    }
+
+    public Sintatico(String s) {
+        try {
+            StringReader stringReader = new StringReader(s);
+            LogicoLexico analizador = new LogicoLexico(stringReader);
+
+            this.lista = analizador.program();
+
+        } catch (ParseException e) {
+            this.lista.clear();
+            this.lista.add("Errro");
+            this.lista.add(e.getMessage());
+            System.out.println(e.getMessage());
+            System.out.println("Erro na analize!!!");
+        }
+    }
+
+}
