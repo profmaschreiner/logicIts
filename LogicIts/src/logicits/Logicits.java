23b571a53d7991f9fd14ced65cd9da7732542ec7
fabio <fabio.henrique.gil@gmail.com>
2016-08-23 16:31:17 -0300

    testes com todas as expressões da lista de equivalencia!

--- LogicIts/src/logicits/Logicits.java
+++ LogicIts/src/logicits/Logicits.java
@@ -15,7 +15,30 @@ import logicits.javaccList.Sintatico;
  */
 public class Logicits {
     public static void main(String[] args) {
-        Sintatico s = new Sintatico("~~p");
+        //Sintatico s = new Sintatico("p");
+        //Sintatico s = new Sintatico("p & p");
+        //Sintatico s = new Sintatico("p | p");
+        //Sintatico s = new Sintatico("p & q");
+        //Sintatico s = new Sintatico("q & p");
+        //Sintatico s = new Sintatico("p | q");
+        //Sintatico s = new Sintatico("q | p");
+        //Sintatico s = new Sintatico("p & (q | r)");
+        //Sintatico s = new Sintatico("p | (q & r)");
+        //Sintatico s = new Sintatico("(p & q) | (p & r)");
+        //Sintatico s = new Sintatico("~~p");
+        //Sintatico s = new Sintatico("~(p & q)");
+        //Sintatico s = new Sintatico("~p | ~q");
+        //Sintatico s = new Sintatico("~(p | q)");
+        //Sintatico s = new Sintatico("~p & ~q");
+        //Sintatico s = new Sintatico("p -> q");
+        //Sintatico s = new Sintatico("~p | q");
+        //Sintatico s = new Sintatico("(p <-> q");
+        //Sintatico s = new Sintatico("(p -> q) & (q -> p)");
+        //Sintatico s = new Sintatico("(p & q) | (~p & ~q)");
+        //Sintatico s = new Sintatico("~q -> ~p");
+        Sintatico s = new Sintatico("(p & q) -> r");
+        //Sintatico s = new Sintatico("p -> (q -> r)");
+        
         if (!"Erro".equals(s.lista.get(0))) {
             for (Iterator token = s.lista.iterator(); token.hasNext();) {
                 Object next = token.next();
